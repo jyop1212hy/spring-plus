daf4c054abfa652142ee08c339d5e86c0cb40b43
jyop1212hy <jyop1212@icloud.com>
2025-12-27 01:03:17 +0900

    테스트 코드 수정

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -1,5 +1,8 @@
 package org.example.expert.domain.todo.controller;
 
+import org.example.expert.config.GlobalExceptionHandler;
+import org.example.expert.config.JwtFilter;
+import org.example.expert.config.JwtUtil;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
@@ -12,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -23,7 +28,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TodoController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class TodoControllerTest {
+
+    @MockBean
+    private JwtUtil jwtUtil;   // ✅ JwtFilter가 주입받을 빈
+
+    @MockBean
+    private JwtFilter jwtFilter;
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,6 +43,7 @@ class TodoControllerTest {
     @MockBean
     private TodoService todoService;
 
+    @WithMockUser
     @Test
     void todo_단건_조회에_성공한다() throws Exception {
         // given
@@ -59,6 +72,7 @@ class TodoControllerTest {
                 .andExpect(jsonPath("$.title").value(title));
     }
 
+    @WithMockUser
     @Test
     void todo_단건_조회_시_todo가_존재하지_않아_예외가_발생한다() throws Exception {
         // given
